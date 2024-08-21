83a1e1dcae555bf4020429613bf7e18af0a77537
oktma <martin.okter@hiveautonomy.no>
2024-08-21 15:59:11 +0200

    Fist successful setup

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,8 +2,8 @@
 
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload example &
+    MONITOR=$m polybar --reload icebar &
   done
 else
-  polybar --reload example &
+  polybar --reload icebar &
 fi
